2284f2f9184b12f471af08139463897e0dd2c854
RAFiqey <RAF91@inbox.ru>
2018-10-30 18:53:48 +0300

    Initial commit

---
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -0,0 +1,34 @@
+package com.example.sweater;
+
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Map;
+
+@Controller
+public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
+
+    @GetMapping("/greeting")
+    public String greeting(
+            @RequestParam(name="name", required=false, defaultValue="World") String name,
+            Map<String, Object> model
+    ) {
+        model.put("name", name);
+        return "greeting";
+    }
+
+    @GetMapping
+    public String main(Map<String, Object> model){
+        messageRepo.findAll();
+
+        model.put("some", "RAFiq pro!");
+        return "main";
+    }
+
+}
